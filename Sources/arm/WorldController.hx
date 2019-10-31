b057bc5dbff18d26e290b680dc5210a83ecb1b33
BlackGoku36 <urjasvisuthar@gmail.com>
2019-11-01 02:15:48 +0530

    adding of resource bugfix

--- Sources/arm/WorldController.hx
+++ Sources/arm/WorldController.hx
@@ -67,16 +67,16 @@ class WorldController extends iron.Trait {
 		var world = WorldController;
 		var canvas = MainCanvasController;
 		houseProp.tt = Scheduler.addTimeTask(function(){
-			if (electricity[0] >= world.houseProp.costE){
-				if (money[0] <= money[1]) money[0] += houseProp.at * houseProp.prodM;
+			if (electricity[0] >= world.houseProp.costE && money[0] <= money[1]){
+				money[0] += houseProp.at * houseProp.prodM;
 				electricity[0] -= houseProp.at * houseProp.costE;
 			}else{
 				//canvas.setWarning("Out of Electricity!");
 			}
 		}, 5, 5);
 		parkProp.tt = Scheduler.addTimeTask(function(){
-			if (electricity[0] >= world.parkProp.costE){
-				if(money[0] <= money[1]) money[0] += parkProp.at * parkProp.prodM;
+			if (electricity[0] >= world.parkProp.costE && money[0] <= money[1]){
+				money[0] += parkProp.at * parkProp.prodM;
 				electricity[0] -= parkProp.at * houseProp.costE;
 			}else{
 				//canvas.setWarning("Out of Electricity!");
@@ -99,24 +99,24 @@ class WorldController extends iron.Trait {
 			// }
 		}, 5, 5);
 		sawmillProp.tt = Scheduler.addTimeTask(function(){
-			if (electricity[0] >= world.sawmillProp.costE){
-				if(woods[0] <= woods[1]) woods[0] += sawmillProp.at * sawmillProp.prodW;
+			if (electricity[0] >= world.sawmillProp.costE && woods[0] <= woods[1]){
+				woods[0] += sawmillProp.at * sawmillProp.prodW;
 				electricity[0] -= sawmillProp.at * sawmillProp.costE;
 			}else{
 				//canvas.setWarning("Out of Electricity!");
 			}
 		}, 5, 5);
 		quarryProp.tt = Scheduler.addTimeTask(function(){
-			if (electricity[0] >= world.quarryProp.costE){
-				if(stones[0] <= stones[1]) stones[0] += quarryProp.at * quarryProp.prodSe;
+			if (electricity[0] >= world.quarryProp.costE && stones[0] <= stones[1]){
+				stones[0] += quarryProp.at * quarryProp.prodSe;
 				electricity[0] -= quarryProp.at * quarryProp.costE;
 			}else{
 				//canvas.setWarning("Out of Electricity!");
 			}
 		}, 5, 5);
 		steelworksProp.tt = Scheduler.addTimeTask(function(){
-			if (electricity[0] >= world.steelworksProp.costE){
-				if (steels[0] <= steels[1]) steels[0] += steelworksProp.at * steelworksProp.prodSl;
+			if (electricity[0] >= world.steelworksProp.costE && steels[0] <= steels[1]){
+				steels[0] += steelworksProp.at * steelworksProp.prodSl;
 				electricity[0] -= steelworksProp.at * steelworksProp.costE;
 			}else{
 				//canvas.setWarning("Out of Electricity!");
